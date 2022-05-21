6723ee5b9ce5e50a504df08803765e9b6262c3c8
Subh-github <subhamkumarsaoo12345@gmail.com>
2022-05-21 20:58:44 +0530

    html css added

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('form.html')
+    return render_template('Home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
