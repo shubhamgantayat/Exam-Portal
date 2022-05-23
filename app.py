7bafc17b6ab7229898c30a310fff29f8513b2d28
Shubham Gantayat <sgantayat9@gmail.com>
2022-05-23 12:55:19 +0530

    minor changes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ config.mongo_db = Operations("ExamPortal", config.logger)
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('question.html')
+    return render_template('Home.html')
 
 
 @app.route('/auth/login', methods=['GET', 'POST'])
