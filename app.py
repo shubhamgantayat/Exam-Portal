78905f5fa0ba5912e3309b8e211a56e45f50a6d0
Shubham Gantayat <sgantayat9@gmail.com>
2022-05-23 11:41:03 +0530

    login logout and session backend added

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
 from py_backend.logger.log_db import Logger
 from py_backend.mongo_db.crud import Operations
 from py_backend.signup.signup_user import Registration
 from py_backend.login.login_user import Validation
 import config
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 config.logger = Logger()
 config.mongo_db = Operations("ExamPortal", config.logger)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('Home.html')
+    return render_template('question.html')
 
 
 @app.route('/auth/login', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html')
+    if not session.get('email'):
+        return render_template('login.html')
+    return redirect('/auth/user')
+
+
+@app.route('/auth/logout', methods=['GET', 'POST'])
+def logout():
+    session["email"] = None
+    session['password'] = None
+    return redirect('/')
 
 
 @app.route('/auth/login-user', methods=['GET', 'POST'])
@@ -26,7 +39,16 @@ def login():
         email = request.form["Email"]
         password = request.form["Password"]
         res = Validation(email, password).check()
+        if res["status"]:
+            session["email"] = email
+            session["password"] = password
+            return redirect('/auth/user')
         return res
+
+
+@app.route('/auth/user', methods=["GET", "POST"])
+def show_user():
+    return session.get("email")
 
 
 @app.route('/auth/signup', methods=['GET', 'POST'])
